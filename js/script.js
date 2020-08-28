93629dde490d3243b3d5a951fa9bbbd9a8c405b6
Anna Agozzino <anna.agozzino91@gmail.com>
2020-08-28 15:00:03 +0200

    WIP creare array numeri PC

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 // Genero 16 numeri da 1 a 100
 //I numeri devono essere tutti diversi
+var numRandomPc = [];
+var i = 0;
 
+while (numRandomPc.lenght < 16) {
+  var numRandom = Math.floor(Math.random() * 100) + 1;
+  numRandomPc.push(numRandom);
+  i++;
+}
+
+console.log(numRandomPc);
 
 //Chiedo all'utente di inserire 84 numeri compresi tra 1 e 100
 //I numeri devono essere tutti diversi
