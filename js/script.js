05a5e36a45e822d92df0f7aa3c7235d6ccb1014f
Anna Agozzino <anna.agozzino91@gmail.com>
2020-08-28 18:38:12 +0200

    Esercizio con funzioni

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
 // Var debug
-var bombe = 10
-var tentativi = 5
+var celle = 100
+var bombe = 16;
+var tentativi = celle - bombe;
+
+//FUNZIONE - Generatore numeri casuali da 1 a 100
+function numGenerator(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + 1;
+};
+
+//FUNZIONE - Check element in array
+function elementInArray(element, array) {
+  return array.indexOf(element);
+}
 // Genero 16 numeri da 1 a 100
 //I numeri devono essere tutti diversi
 var listaNumRandomPc = [];
 
 for (var i = 0; listaNumRandomPc.length < bombe; i++) {
-  var numRandom = Math.floor(Math.random() * 100) + 1;
-  if (listaNumRandomPc.indexOf(numRandom) == -1) {
+  var numRandom = numGenerator(1, 100);
+  var checkElement = elementInArray(numRandom, listaNumRandomPc);
+  if (checkElement == -1) {
     listaNumRandomPc.push(numRandom);
   };
   console.log(numRandom);
@@ -27,11 +39,13 @@ var i = 0;
 // && listaNumRandomPc.indexOf(numeroUtente) == -1
 while (listaNumUtente.length < tentativi) {
   numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 100"));
-  if (numeroUtente < 1 || numeroUtente > 100) {
+  var checkDouble = elementInArray(numeroUtente, listaNumUtente);
+  var checkBomb = elementInArray(numeroUtente, listaNumRandomPc);
+  if (numeroUtente < 1 || numeroUtente > 100 || isNaN(numeroUtente) == true) {
     alert("Attenzione: Inserisci un numero tra 1 e 100!!!");
-  } else if (listaNumUtente.indexOf(numeroUtente) >= 0) {
+  } else if (checkDouble >= 0) {
     alert("Attenzione: numero già inserito!!!");
-  } else if (listaNumRandomPc.indexOf(numeroUtente) != -1) {
+  } else if (checkBomb != -1) {
     break;
   }else {
     listaNumUtente.push(numeroUtente);
@@ -42,7 +56,7 @@ console.log(listaNumUtente);
 
 //Comunicare il punteggio == lunghezza lista numeriUtente
   // Se listaNumUtente.length == tentativi -> HAI VINTO
-  // Altrimenti -> HAI PERSO - il tuo livello è listaNumUtente.lenght -1
+  // Altrimenti -> HAI PERSO - il tuo livello è listaNumUtente.lenght
 if (listaNumUtente.length == tentativi) {
   console.log("HAI VINTO!!!");
 } else {
