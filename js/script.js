a4a8083c78a8a577caffee1d83dba324445c7394
Anna Agozzino <anna.agozzino91@gmail.com>
2020-08-28 14:57:55 +0200

    Inizio progetto

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Genero 16 numeri da 1 a 100
+//I numeri devono essere tutti diversi
+
+
+//Chiedo all'utente di inserire 84 numeri compresi tra 1 e 100
+//I numeri devono essere tutti diversi
+
+//Continuo a chiedere all'utente un numero nuovo finché
+// numeroUtente == numeroPC
+// lunghezza lista numeriUtente == 84
+
+//Comunicare il punteggio == lunghezza lista numeriUtente
