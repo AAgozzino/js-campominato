4127ef8dc3732b33a9e9f7769d9ae20af4f28f3b
Anna Agozzino <anna.agozzino91@gmail.com>
2020-08-28 15:06:49 +0200

    Array 16 numeri per PC

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
 // Genero 16 numeri da 1 a 100
 //I numeri devono essere tutti diversi
 var numRandomPc = [];
-var i = 0;
 
-while (numRandomPc.lenght < 16) {
+for (var i = 0; numRandomPc.length < 10; i++) {
   var numRandom = Math.floor(Math.random() * 100) + 1;
-  numRandomPc.push(numRandom);
-  i++;
-}
-
+  if (numRandomPc.indexOf(numRandom) == -1) {
+    numRandomPc.push(numRandom);
+  };
+  console.log(numRandom);
+};
 console.log(numRandomPc);
+// while (numRandomPc.lenght < 16) {
+//   var numRandom = Math.floor(Math.random() * 100) + 1;
+//   numRandomPc.push(numRandom);
+//   i++;
+// }
+
+//console.log(numRandomPc);
 
 //Chiedo all'utente di inserire 84 numeri compresi tra 1 e 100
 //I numeri devono essere tutti diversi
