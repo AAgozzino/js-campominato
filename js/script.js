5aeb9082ed623479cbdd32a9c500e39ada369f73
Anna Agozzino <anna.agozzino91@gmail.com>
2020-08-29 12:36:42 +0200

    Campo minato con livelli

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-// Var debug
-var celle = 100
-var bombe = 16;
+// Var debug - livello
+var livello = parseInt(prompt("Scegli il tuo livello di difficoltà inserendo 0, 1 o 2"));
+var celle;
+var bombe = 16
+
+if (livello == 0) {
+  celle = 100;
+} else if (livello == 1) {
+  celle = 80;
+} else {
+  celle = 50;
+}
 var tentativi = celle - bombe;
 
 //FUNZIONE - Generatore numeri casuali da 1 a 100
@@ -17,7 +26,7 @@ function elementInArray(element, array) {
 var listaNumRandomPc = [];
 
 for (var i = 0; listaNumRandomPc.length < bombe; i++) {
-  var numRandom = numGenerator(1, 100);
+  var numRandom = numGenerator(1, celle);
   var checkElement = elementInArray(numRandom, listaNumRandomPc);
   if (checkElement == -1) {
     listaNumRandomPc.push(numRandom);
@@ -28,21 +37,20 @@ console.log(listaNumRandomPc);
 
 
 //Continuo a chiedere all'utente un numero tra 1 e 100 finché
-// numeroUtente == numeroPC
 // lunghezza lista numeriUtente == 84
   //I numeri devono essere tutti diversi
   //I numeri devono essere compresi tra 1 e 100
+  // numeroUtente == numeroPC -> break
 var listaNumUtente = [];
 var numeroUtente
 var i = 0;
 
-// && listaNumRandomPc.indexOf(numeroUtente) == -1
 while (listaNumUtente.length < tentativi) {
-  numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 100"));
+  numeroUtente = parseInt(prompt("Inserisci un numero da 1 a " + celle));
   var checkDouble = elementInArray(numeroUtente, listaNumUtente);
   var checkBomb = elementInArray(numeroUtente, listaNumRandomPc);
-  if (numeroUtente < 1 || numeroUtente > 100 || isNaN(numeroUtente) == true) {
-    alert("Attenzione: Inserisci un numero tra 1 e 100!!!");
+  if (numeroUtente < 1 || numeroUtente > celle || isNaN(numeroUtente) == true) {
+    alert("Attenzione: Inserisci un numero tra 1 e " + celle);
   } else if (checkDouble >= 0) {
     alert("Attenzione: numero già inserito!!!");
   } else if (checkBomb != -1) {
